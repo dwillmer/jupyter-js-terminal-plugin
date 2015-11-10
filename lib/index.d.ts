112eaf52603cb02b7824ce23bab4220f83edd74c
Dave Willmer <dave.willmer@gmail.com>
2015-11-10 14:45:14 +0000

    WIP

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,2 @@
 export declare function menuLoader(extension: any): Promise<any>;
+export declare function uiLoader(extension: any): Promise<any>;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@
 // Distributed under the terms of the Modified BSD License.
 'use strict';
 
+// import {
+//   TerminalWidget
+// } from 'jupyter-js-terminal';
+
+import {
+  Tab
+} from 'phosphor-tabs';
+
+
 var MENU = {
   items: [
     {
@@ -11,6 +20,17 @@ var MENU = {
   ]
 };
 
-export function menuLoader(extension:any): Promise<any> {
+export
+function menuLoader(extension:any): Promise<any> {
   return Promise.resolve(MENU);
+}
+
+
+export
+function uiLoader(extension: any): Promise<any> {
+  // var ui = {
+  //   items: [new TerminalWidget()],
+  //   tabs: [new Tab('Terminal')]
+  // };
+  return Promise.resolve(void 0);
 }
