0eabeeed4a9312a61d8410ab31bc74a7fd9f6699
Dave Willmer <dave.willmer@gmail.com>
2015-11-10 16:45:57 +0000

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@
 // Distributed under the terms of the Modified BSD License.
 'use strict';
 
-// import {
-//   TerminalWidget
-// } from 'jupyter-js-terminal';
 
 import {
   Tab
@@ -23,14 +20,4 @@ var MENU = {
 export
 function menuLoader(extension:any): Promise<any> {
   return Promise.resolve(MENU);
-}
-
-
-export
-function uiLoader(extension: any): Promise<any> {
-  // var ui = {
-  //   items: [new TerminalWidget()],
-  //   tabs: [new Tab('Terminal')]
-  // };
-  return Promise.resolve(void 0);
 }
